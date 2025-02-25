e6073c77c219430f9d0ca730f2d7fe9069621948
Damian <130514665+dfount@users.noreply.github.com>
2025-02-25 01:39:24 -0600

    Delete Part_1/Version 3/dfount Basic-OS-Simulator main Part_1-Version 3 (final) directory

--- Part_1/Version 3/dfount Basic-OS-Simulator main Part_1-Version 3 (final)/auth.h
+++
@@ -1,9 +0,0 @@
-#ifndef AUTH_H
-#define AUTH_H
-
-#include <string>
-using namespace std;
-
-bool authenticateUser(const string &validUser, const string &validPass);
-
-#endif
--- Part_1/Version 3/dfount Basic-OS-Simulator main Part_1-Version 3 (final)/auth_handler.h
+++
@@ -1,6 +0,0 @@
-#ifndef AUTH_HANDLER_H
-#define AUTH_HANDLER_H
-
-void runAuthentication();
-
-#endif
